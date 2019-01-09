c4021c5ba000d2a51850853d6a34a71680a2ae7e
shimaa <shimamostafa344@gmail.com>
2019-01-09 14:25:35 +0200

    [ADD] add doaa module

---
+++ quotation_order_temp_edit/__manifest__.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "quotation_order_temp_edit",
+
+    'summary': """
+        quotation_order_temp_edit""",
+
+    'description': """
+        quotation_order_temp_editr
+    """,
+
+    'author': "ITsys-Corportion Doaa Khaled",
+    'website': "http://www.it-syscorp.com",
+
+    'category': 'Sale',
+    'version': '0.1',
+
+    'depends': ['base','sale',],
+
+    # always loaded
+    'data': [
+        # 'security/ir.model.access.csv',
+        'views/quotation_temp_edit.xml',
+    ],
+
+
+}
