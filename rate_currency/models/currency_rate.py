afeede8ace0db7a64d5c1dfe23a81d2540be1bb0
shimaa <shimamostafa344@gmail.com>
2019-01-20 17:10:08 +0200

    [ADD] add Eman work

--- rate_currency/models/currency_rate.py
+++ rate_currency/models/currency_rate.py
@@ -6,7 +6,7 @@ class CurrencyRate(models.Model):
     _inherit = "res.currency.rate"
 
 
-    rate = fields.Float(digits=(12,20), help='The rate of the currency to the currency of rate 1')
+    rate = fields.Float(digits=(12,16), help='The rate of the currency to the currency of rate 1')
 
 
 
