40d3821f990186669370db1c83174cb33f09e0cf
shimaa <shimamostafa344@gmail.com>
2019-01-22 13:43:29 +0200

    [UPDATE] update Eman work

--- rate_currency/models/currency_rate.py
+++ rate_currency/models/currency_rate.py
@@ -8,6 +8,12 @@ class CurrencyRate(models.Model):
 
     rate = fields.Float(digits=(12,16), help='The rate of the currency to the currency of rate 1')
 
+class Currency(models.Model):
+    _inherit= "res.currency"
+    rate = fields.Float(compute='_compute_current_rate', string='Current Rate', digits=(12,16),
+                        help='The rate of the currency to the currency of rate 1.')
+
+
 
 
 
