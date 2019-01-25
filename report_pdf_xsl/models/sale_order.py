9ec03ef46489f2ef5f6e86fa792b64bd04b1b1f6
shimaa <shimamostafa344@gmail.com>
2019-01-25 14:18:13 +0200

    [ADD] add Eman work

---
+++ report_pdf_xsl/models/sale_order.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+
+from odoo import models, fields, api
+from odoo.exceptions import ValidationError
+
+
+class SaleOrderLine(models.Model):
+    _inherit = 'sale.order.line'
+    delivery_note=fields.Text()
+
+
