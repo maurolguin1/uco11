d35e631c3d1fa7126b342fbc9ba135ebd9dec310
shimaa <shimamostafa344@gmail.com>
2019-03-21 10:54:14 +0200

    [ADD] add eman work

--- product_manager_security/models/sale_order.py
+++ product_manager_security/models/sale_order.py
@@ -2,11 +2,60 @@
 
 from odoo import models, fields, api
 from odoo.exceptions import AccessError, MissingError, ValidationError, UserError
+from openerp import SUPERUSER_ID
+
+class PurchaseOrder(models.Model):
+    _inherit = 'purchase.order'
+
+    @api.multi
+    @api.returns('self', lambda value: value.id)
+    def message_post(self, body='', subject=None, message_type='notification',
+                     subtype=None, parent_id=False, attachments=None,
+                     content_subtype='html', **kwargs):
+        res = super(PurchaseOrder, self).message_post(body=body, subject=subject, message_type=message_type,
+                                                  subtype=subtype, parent_id=parent_id, attachments=attachments,
+                                                  content_subtype=content_subtype, **kwargs)
+
+        if self._context.get('uid'):
+            if self._context['uid'] != self.env.user.id:
+                partner_id = self.env['res.users'].browse(self._context['uid']).partner_id.id
+                res.write({'author_id': partner_id})
+        return res
 
 
 class SaleOrder(models.Model):
     _inherit = 'sale.order'
 
+    @api.multi
+    @api.returns('self', lambda value: value.id)
+    def message_post(self, body='', subject=None, message_type='notification',
+                     subtype=None, parent_id=False, attachments=None,
+                     content_subtype='html', **kwargs):
+        res = super(SaleOrder, self).message_post(body=body, subject=subject, message_type=message_type,
+                                                  subtype=subtype, parent_id=parent_id, attachments=attachments,
+                                                  content_subtype=content_subtype, **kwargs)
+
+        if self._context.get('uid'):
+            if self._context['uid'] != self.env.user.id:
+                partner_id = self.env['res.users'].browse(self._context['uid']).partner_id.id
+                res.write({'author_id': partner_id})
+        return res
+
+
+
+    product_manager=fields.Many2many('res.users',relation='sale_product_manger_rel',column1='sale_id',column2='manager_id',compute='ch_mana',store=True)
+    @api.depends('order_line.managers_id')
+    def ch_mana(self):
+
+        lis_man=[]
+
+        for rec in self:
+            if rec.order_line:
+                for lin in rec.order_line:
+                  for l in lin.managers_id:
+                      lis_man.append(l.id)
+
+            rec.product_manager=lis_man
 
 
     @api.model
@@ -82,7 +131,7 @@ class SaleOrderLine(models.Model):
                 if rec.product_id.categ_id.managers_id:
                     rec.managers_id = rec.product_id.categ_id.managers_id
 
-    managers_id = fields.Many2many('res.users', compute="_get_prodcut_managers", string='Product Managers', store=True)
+    managers_id = fields.Many2many('res.users',relation='sale_line_product_manger_rel',column1='line_id',column2='manager_id', compute="_get_prodcut_managers", string='Product Managers', store=True)
 
     state_confirm = fields.Selection([
         ('confirm', 'Confirm')], store=True)
